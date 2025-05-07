5db98fe7c72950b85531db2969bb445d51ab0d95
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2025-05-07 13:03:09 +0200

    ci: add cobertura coverage output
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,6 @@ module.exports = {
   testRegex: "(tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
   testPathIgnorePatterns: ["/dist/", "/node_modules/"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  coverageReporters: ["cobertura", "lcov"],
   collectCoverage: true,
 };
--- tests/lib/metadata.test.ts
+++ tests/lib/metadata.test.ts
@@ -1,82 +1,82 @@
 import {
-  V1DaemonSet,
-  V1Deployment,
-  V1StatefulSet,
+    V1DaemonSet,
+    V1Deployment,
+    V1StatefulSet,
 } from "@kubernetes/client-node";
 import { enforceAnnotations } from "../../src/lib/metadata/enforceAnnotations";
 import { V1AdmissionRequest } from "@jspolicy/types";
 
 describe("enforceAnnotations", () => {
-  test("should handle empty metadata", () => {
-    // prepare
-    const object: V1DaemonSet = {
-      metadata: {},
-    };
-    const request: V1AdmissionRequest = {
-      object: object,
-    } as V1AdmissionRequest;
-    const annotations = {
-      "test.key": "value",
-    };
+    test("should handle empty metadata", () => {
+        // prepare
+        const object: V1DaemonSet = {
+            metadata: {},
+        };
+        const request: V1AdmissionRequest = {
+            object: object,
+        } as V1AdmissionRequest;
+        const annotations = {
+            "test.key": "value",
+        };
 
-    // test
-    const response = enforceAnnotations(request, annotations) as V1DaemonSet;
+        // test
+        const response = enforceAnnotations(request, annotations) as V1DaemonSet;
 
-    // validate
-    expect(response).toBeDefined();
-    expect(response.metadata?.annotations).toEqual({
-      "test.key": "value",
+        // validate
+        expect(response).toBeDefined();
+        expect(response.metadata?.annotations).toEqual({
+            "test.key": "value",
+        });
     });
-  });
 
-  test("should handle empty annotations", () => {
-    // prepare
-    const object: V1Deployment = {
-      metadata: {
-        annotations: {},
-      },
-    };
-    const request: V1AdmissionRequest = {
-      object: object,
-    } as V1AdmissionRequest;
-    const annotations = {
-      "test.key": "value",
-    };
+    test("should handle empty annotations", () => {
+        // prepare
+        const object: V1Deployment = {
+            metadata: {
+                annotations: {},
+            },
+        };
+        const request: V1AdmissionRequest = {
+            object: object,
+        } as V1AdmissionRequest;
+        const annotations = {
+            "test.key": "value",
+        };
 
-    // test
-    const response = enforceAnnotations(request, annotations) as V1Deployment;
+        // test
+        const response = enforceAnnotations(request, annotations) as V1Deployment;
 
-    // validate
-    expect(response).toBeDefined();
-    expect(response.metadata?.annotations).toEqual({
-      "test.key": "value",
+        // validate
+        expect(response).toBeDefined();
+        expect(response.metadata?.annotations).toEqual({
+            "test.key": "value",
+        });
     });
-  });
 
-  test("should retain existing annotations", () => {
-    // prepare
-    const object: V1StatefulSet = {
-      metadata: {
-        annotations: {
-          "example.com/key": "test",
-        },
-      },
-    };
-    const request: V1AdmissionRequest = {
-      object: object,
-    } as V1AdmissionRequest;
-    const annotations = {
-      "test.key": "value",
-    };
+    test("should retain existing annotations", () => {
+        // prepare
+        const object: V1StatefulSet = {
+            metadata: {
+                annotations: {
+                    "example.com/key": "test",
+                },
+            },
+        };
+        const request: V1AdmissionRequest = {
+            object: object,
+        } as V1AdmissionRequest;
+        const annotations = {
+            "test.key": "value",
+        };
 
-    // test
-    const response = enforceAnnotations(request, annotations) as V1StatefulSet;
+        // test
+        const response = enforceAnnotations(request, annotations) as V1StatefulSet;
 
-    // validate
-    expect(response).toBeDefined();
-    expect(response.metadata?.annotations).toEqual({
-      "test.key": "value",
-      "example.com/key": "test",
+        // validate
+        expect(response).toBeDefined();
+        expect(response.metadata?.annotations).toEqual({
+            "test.key": "value",
+            "example.com/key": "test",
+        });
     });
-  });
 });
